76e425fc58c5d820e8050203106ee614c85697a1
jmatte123 <joseph1matteson@gmail.com>
2017-11-01 20:56:49 -0500

    db

---
+++ Q1Question1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quiz 1</title>
+    <link href="Questions.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+ <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>"> <!-- submit the form to the server-->
+
+ </form>
+</body>
+</html>
