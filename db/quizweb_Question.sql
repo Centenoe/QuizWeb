2c634a34d96753bcec1d37ff3841c150bf2a5423
jmatte123 <joseph1matteson@gmail.com>
2017-11-07 13:45:59 -0600

    stupid mistake fix

--- db/quizweb_Question.sql
+++ db/quizweb_Question.sql
@@ -4,7 +4,7 @@ CREATE TABLE quizweb.Question
     Quiz_id int(11) NOT NULL,
     Question_S varchar(255) NOT NULL,
     CONSTRAINT `PRIMARY` PRIMARY KEY (Question_id, Quiz_id),
-    CONSTRAINT question_ibfk_1 FOREIGN KEY (Quiz_id) REFERENCES quiz
+    CONSTRAINT question_ibfk_1 FOREIGN KEY (Quiz_id) REFERENCES quiz(Quiz_id)
 );
 CREATE INDEX Quiz_id ON quizweb.Question (Quiz_id);
 INSERT INTO quizweb.Question (Question_id, Quiz_id, Question_S) VALUES (1, 1, 'How many States are in the United States?');
